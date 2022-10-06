4d9cc5e232e35cf30f69e4b84b96b8d44f91825f
Riccardo Gulin <riccardo@nucleode.com>
2022-10-06 12:31:31 +0200

    Adds error handlers

--- src/api/books/index.js
+++ src/api/books/index.js
@@ -17,6 +17,9 @@ import { fileURLToPath } from "url"
 import { dirname, join } from "path"
 import uniqid from "uniqid"
 import fs from "fs"
+import createHttpError from "http-errors"
+
+const { BadRequest, NotFound } = createHttpError
 
 const booksRouter = express.Router()
 
@@ -34,60 +37,100 @@ const anotherStupidMiddleware = (req, res, next) => {
   next()
 }
 
-booksRouter.post("/", (req, res) => {
-  const newBook = { ...req.body, createdAt: new Date(), id: uniqid() }
+booksRouter.post("/", (req, res, next) => {
+  try {
+    const newBook = { ...req.body, createdAt: new Date(), id: uniqid() }
 
-  const books = getBooks()
+    const books = getBooks()
 
-  books.push(newBook)
+    books.push(newBook)
 
-  writeBooks(books)
+    writeBooks(books)
 
-  res.status(201).send({ id: newBook.id })
-})
-
-booksRouter.get("/", anotherStupidMiddleware, (req, res) => {
-  const books = getBooks()
-
-  if (req.query && req.query.category) {
-    const filteredBooks = books.filter(
-      book => book.category === req.query.category
-    )
-    res.send(filteredBooks)
-  } else {
-    res.send(books)
+    res.status(201).send({ id: newBook.id })
+  } catch (error) {
+    next(error)
   }
 })
 
-booksRouter.get("/:bookId", (req, res) => {
-  const books = getBooks()
-  const foundBook = books.find(book => book.id === req.params.bookId)
-  res.send(foundBook)
+booksRouter.get("/", anotherStupidMiddleware, (req, res, next) => {
+  try {
+    // throw new Error("KABOOOOOOOOOOOOOOOOOOOOOOOOM!")
+    const books = getBooks()
+
+    if (req.query && req.query.category) {
+      const filteredBooks = books.filter(
+        book => book.category === req.query.category
+      )
+      res.send(filteredBooks)
+    } else {
+      res.send(books)
+    }
+  } catch (error) {
+    next(error) // This line here is going to send the error to the first error handler used in server.js
+  }
 })
 
-booksRouter.put("/:bookId", (req, res) => {
-  const books = getBooks()
-
-  const index = books.findIndex(book => book.id === req.params.bookId)
-
-  const oldBook = books[index]
-
-  const updatedBook = { ...oldBook, ...req.body, updatedAt: new Date() }
-  books[index] = updatedBook
-
-  writeBooks(books)
-
-  res.send(updatedBook)
+booksRouter.get("/:bookId", (req, res, next) => {
+  try {
+    const books = getBooks()
+    const foundBook = books.find(book => book.id === req.params.bookId)
+    if (foundBook) {
+      res.send(foundBook)
+    } else {
+      // next(createHttpError(404, `Book with id ${req.params.bookId} not found!`))
+      next(NotFound(`Book with id ${req.params.bookId} not found!`))
+      // this is going to create an error with status code 404 and a message and it is going to send it to the first error handler
+      /*  res
+        .status(404)
+        .send({ message: `Book with id ${req.params.bookId} not found!` }) */
+    }
+  } catch (error) {
+    next(error)
+  }
 })
 
-booksRouter.delete("/:bookId", (req, res) => {
-  const books = getBooks()
+booksRouter.put("/:bookId", (req, res, next) => {
+  try {
+    const books = getBooks()
 
-  const remaingBooks = books.filter(book => book.id !== req.params.bookId)
+    const index = books.findIndex(book => book.id === req.params.bookId)
 
-  writeBooks(remaingBooks)
+    if (index !== -1) {
+      const oldBook = books[index]
 
-  res.status(204).send()
+      const updatedBook = { ...oldBook, ...req.body, updatedAt: new Date() }
+      books[index] = updatedBook
+
+      writeBooks(books)
+
+      res.send(updatedBook)
+    } else {
+      next(createHttpError(404, `Book with id ${req.params.bookId} not found!`))
+      // next(NotFound, `Book with id ${req.params.bookId} not found!`)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+booksRouter.delete("/:bookId", (req, res, next) => {
+  try {
+    const books = getBooks()
+
+    const remaingBooks = books.filter(book => book.id !== req.params.bookId)
+
+    if (books.length !== remaingBooks.length) {
+      writeBooks(remaingBooks)
+
+      res.status(204).send()
+    } else {
+      next(createHttpError(404, `Book with id ${req.params.bookId} not found!`))
+      // next(NotFound, `Book with id ${req.params.bookId} not found!`)
+    }
+  } catch (error) {
+    next(error)
+  }
 })
 
 export default booksRouter
--- src/server.js
+++ src/server.js
@@ -2,38 +2,54 @@
 import express from "express" // NEW SYNTAX (you can use this only if type:"module" is added on package.json)
 import listEndpoints from "express-list-endpoints"
 import cors from "cors"
+import createHttpError from "http-errors"
 import usersRouter from "./api/users/index.js"
 import booksRouter from "./api/books/index.js"
+import {
+  badRequestHandler,
+  genericErrorHandler,
+  notFoundHandler,
+  unauthorizedHandler,
+} from "./errorHandlers.js"
 
 const server = express()
 const port = 3001
 
 // *************************************** MIDDLEWARES *************************************
 
+/* const { Unauthorized } = createHttpError
+
 const loggerMiddleware = (req, res, next) => {
   console.log(
     `Request method: ${req.method} -- request url: ${req.url} -- ${new Date()}`
   )
-  req.user = "Miles"
+  req.user = "Riccardo"
   next()
 }
 
 const policeOfficer = (req, res, next) => {
   if (req.user === "Riccardo") {
-    res.status(401).send({ message: "Riccardos are not allowed!" })
+    next(Unauthorized("Riccardos are not allowed!"))
   } else {
     next()
   }
 }
 
 server.use(loggerMiddleware)
-server.use(policeOfficer)
+server.use(policeOfficer) */
 server.use(cors())
 server.use(express.json()) // If you don't add this line BEFORE the endpoints, all requests' bodies will be UNDEFINED
 
 // ************************************* ENDPOINTS ******************************************
-server.use("/users", loggerMiddleware, usersRouter) // /users will be the prefix that all the endpoints in the usersRouter will have
+server.use("/users", usersRouter) // /users will be the prefix that all the endpoints in the usersRouter will have
 server.use("/books", booksRouter)
+
+// ********************************** ERROR HANDLERS ****************************************
+
+server.use(badRequestHandler)
+server.use(unauthorizedHandler)
+server.use(notFoundHandler)
+server.use(genericErrorHandler)
 
 server.listen(port, () => {
   console.table(listEndpoints(server))
